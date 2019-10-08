4f673d46ffc2c37356e89e4924fa4ec20fed6ceb
Kevin <stradtkt22@gmail.com>
2019-10-08 05:08:57 -0400

    Added the recommender systems

--- recommender_systems/recommender_systems.py
+++ recommender_systems/recommender_systems.py
@@ -30,3 +30,32 @@ ratings['num of ratings'].hist(bins=70)
 ratings['rating'].hist(bins=70)
 
 sns.jointplot(x='rating', y='num of ratings', data=ratings, alpha=0.5)
+
+moviemat = df.pivot_table(index='user_id', columns='title', values='rating')
+moviemat.head()
+
+ratings.sort_values('num of ratings', ascending=False).head(10)
+
+starwars_user_ratings = moviemat['Star Wars (1977)']
+liarliar_user_ratings = moviemat['Liar Liar (1997)']
+similar_to_starwars = moviemat.corrwith(starwars_user_ratings)
+similar_to_liarliar = moviemat.corrwith(liarliar_user_ratings)
+
+corr_starwars = pd.DataFrame(similar_to_starwars, columns=['Correlation'])
+corr_starwars.dropna(inplace=True)
+corr_starwars.head()
+
+corr_starwars.sort_values('Correlation', ascending=False).head(10)
+
+corr_starwars = corr_starwars.join(ratings['num of ratings'])
+corr_starwars.head()
+
+corr_starwars[corr_starwars['num of ratings'] > 100].sort_values('Correlation', ascending=False).head()
+
+corr_liarliar = pd.DataFrame(similar_to_liarliar, columns=['Correlation'])
+corr_liarliar
+
+corr_liarliar = corr_liarliar.join(ratings['num of ratings'])
+corr_liarliar
+
+corr_liarliar[corr_liarliar['num of ratings'] > 100].sort_values('Correlation', ascending=False).head()
